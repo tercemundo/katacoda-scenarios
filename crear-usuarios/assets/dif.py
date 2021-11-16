fe569bbccdba40d3da8ff519146085f381cc5846
Marcelo Guazzardo <mguazzardo76@gmail.com>
2021-11-16 01:35:32 +0000

    crearusuarios

---
+++ crear-usuarios/assets/dif.py
@@ -0,0 +1,23 @@
+import os
+listas = []
+users = []
+salida=os.popen("cat /etc/passwd")
+for lineas in salida.readlines():
+    existentes=lineas.split(":")
+    miembros=existentes[0]
+    listas.append(miembros)
+
+#print(listas)
+
+with open('usuarios.txt') as f:
+    lines = f.read().splitlines()
+#print(lines)
+
+s = set(listas)
+temp3 = [x for x in lines if x not in s]
+
+if len(temp3) > 0:
+    print("hay un user de mas")
+else:
+    print("Todo bien")
+    os.popen("echo ok > salida.txt")
