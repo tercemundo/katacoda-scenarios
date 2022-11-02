0ba000285c6ab7c74795ec77863e2ab1bb6b9162
Marcelo Guazzardo <tercermundo19@gmail.com>
2022-11-01 23:35:44 -0300

    terra

---
+++ terraform/assets/container.tf
@@ -0,0 +1,12 @@
+resource "docker_container" "hello_world" {
+  name  = "cduser_hello_world"
+  image = "tutum/hello-world"
+
+# Especificamos el puerto del contenedor que vamos a exponer
+
+  ports {
+    internal = 80
+    external = 80
+   }
+}
+
