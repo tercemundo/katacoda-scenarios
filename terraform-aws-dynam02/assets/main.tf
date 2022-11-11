412f71ef4d5a1a61e80071ae1c6af5b496f6ee3a
Marcelo Guazzardo <tercermundo19@gmail.com>
2022-11-11 16:19:34 -0300

    dyna02

---
+++ terraform-aws-dynam02/assets/main.tf
@@ -0,0 +1,11 @@
+resource "aws_dynamodb_table" "project_sapphire_user_data" {
+  name           = "userdata"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "UserId"
+
+  attribute {
+    name = "Name"
+    type = "S"
+  }
+}
+
