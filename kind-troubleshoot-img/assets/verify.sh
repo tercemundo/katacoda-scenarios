825c5d7103387e14927c33264195b926845bea7b
Marcelo Guazzardo <mguazzardo76@gmail.com>
2023-02-06 01:34:30 -0300

    .

--- kind-troubleshoot-img/assets/verify.sh
+++ kind-troubleshoot-img/assets/verify.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-echo -e "Checking Objectives..."
+echo -e "Chequeando objetivos..."
 OBJECTIVE_NUM=0
 function printresult {
   ((OBJECTIVE_NUM+=1))
-  echo -e "\n----- Checking Objective $OBJECTIVE_NUM -----"
+  echo -e "\n----- Chequeando objetivo $OBJECTIVE_NUM -----"
   echo -e "----- $1"
   if [ $2 -eq 0 ]; then
-      echo -e "      \033[0;32m[COMPLETE]\033[0m Congrats! This objective is complete!"
+      echo -e "      \033[0;32m[COMPLETE]\033[0m Congrats! Terminaste este lab!"
   else
-      echo -e "      \033[0;31m[INCOMPLETE]\033[0m This objective is not yet completed!"
+      echo -e "      \033[0;31m[INCOMPLETE]\033[0m OH NO. Este lab no esta terminado!"
   fi
 }
 
 expected="nginx:stable"
 actual=$(kubectl get deployment qqmelo -o jsonpath='{.spec.template.spec.containers[0].image}' 2>/dev/null)
 [[ "$actual" = "$expected" ]]
-printresult "Fix a Deployment with an incorrect image name." $?
+printresult "Arregle la imagen." $?
 
